f467efb6453c2a8e646db6f775176420b6987cc9
Benjamin Gee <benjamin.n.gee@gmail.com>
2024-04-16 21:36:44 -0600

    Revert "Inital react app" - switching to use Parcel
    This reverts commit 6b9e0ddc29bbb78d4b9016c900edbbd64561908a.

--- src/components/Popover/Popover.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Popover } from './Popover';
-
-test('renders learn react link', () => {
-  render(<Popover/>);
-  const linkElement = screen.getByText("Welcome to backbone");
-  expect(linkElement).toBeInTheDocument();
-});
